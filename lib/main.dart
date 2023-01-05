1193a286a6c9f363d090d062333b8e8e60a8a836
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-01-05 19:09:24 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(child: TextToSpeech()),
+      ),
+    );
+  }
+}
+
+class TextToSpeech extends StatelessWidget {
+  TextToSpeech({Key? key}) : super(key: key);
+
+  final FlutterTts flutterTts = FlutterTts();
+  final TextEditingController textEditingController = TextEditingController();
+
+  speak(String text) async {
+    await flutterTts.setLanguage("en-Us");
+    await flutterTts.setPitch(1.1);
+    await flutterTts.speak(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            TextFormField(controller: textEditingController),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              child: const Text("Start Text To Speech"),
+              onPressed: () => speak(textEditingController.text),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
